6d49b47ae753bf4af59064a6a4ec647a09476ddd
TikeshKardate <tikesh.kardate@gmail.com>
2025-02-22 12:03:18 +0530

    [Tikesh] Add. Spring hello application by request param

--- src/main/java/com/rest_api/restapicalls/UserController.java
+++ src/main/java/com/rest_api/restapicalls/UserController.java
@@ -14,9 +14,12 @@ public class UserController {
     }
 
     //Method to return hello from Request Parameter
+    //http://localhost:8080/query?name=tikesh
     @GetMapping("/query")
     public String sayHello(@RequestParam(defaultValue = "Tikesh") String name){
         return "Hello "+ name +" " + "from BridgeLabz";
     }
+
+
 
 }
