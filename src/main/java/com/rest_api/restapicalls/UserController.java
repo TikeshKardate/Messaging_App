9339a26d0946db46c9572658b39d2bad73a70fd2
TikeshKardate <tikesh.kardate@gmail.com>
2025-02-22 18:40:39 +0530

    [Tikesh] Add. Messaging application by Path Variable

--- src/main/java/com/rest_api/restapicalls/UserController.java
+++ src/main/java/com/rest_api/restapicalls/UserController.java
@@ -1,6 +1,7 @@
 package com.rest_api.restapicalls;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,16 @@ public class UserController {
     @GetMapping("/query")
     public String sayHello(@RequestParam(defaultValue = "Tikesh") String name){
         return "Hello "+ name +" " + "from BridgeLabz";
+    }
+
+    //Method UC3 using the Path variable to return the message with name of the user
+    //Use http://localhost:8080/display/name
+    @GetMapping({"/display/{name}" , "/display"})
+    public String DisplayHello(@PathVariable (required = false) String name){
+        if (name == null){
+            name = "Tikesh";
+        }
+        return "Hello "+name+ " from BridgeLabz!";
     }
 
 }
