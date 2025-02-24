f380fc36964dbc7c606a157e73ec02ecaa929ca7
TikeshKardate <tikesh.kardate@gmail.com>
2025-02-24 10:54:18 +0530

    [Tikesh] Add. Messaging application by Post Method

---
+++ src/main/java/com/rest_api/restapicalls/DtoUser.java
@@ -0,0 +1,19 @@
+package com.rest_api.restapicalls;
+
+public class DtoUser {
+
+    String firstName;
+    String lastName;
+
+    public DtoUser(String f,String l){
+        firstName=f;
+        lastName=l;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+}
+
--- src/main/java/com/rest_api/restapicalls/UserController.java
+++ src/main/java/com/rest_api/restapicalls/UserController.java
@@ -1,34 +1,38 @@
 package com.rest_api.restapicalls;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
     //Method to return
-    @GetMapping ("/hello")
-    public String sayHello(){
+    @GetMapping("/hello")
+    public String sayHello() {
         return "Hello from BridgeLabz";
     }
 
     //Method to return hello from Request Parameter
     //http://localhost:8080/query?name=tikesh
     @GetMapping("/query")
-    public String sayHello(@RequestParam(defaultValue = "Tikesh") String name){
-        return "Hello "+ name +" " + "from BridgeLabz";
+    public String sayHello(@RequestParam(defaultValue = "Tikesh") String name) {
+        return "Hello " + name + " " + "from BridgeLabz";
     }
 
     //Method UC3 using the Path variable to return the message with name of the user
     //Use http://localhost:8080/display/name
-    @GetMapping({"/display/{name}" , "/display"})
-    public String DisplayHello(@PathVariable (required = false) String name){
-        if (name == null){
+    @GetMapping({"/display/{name}", "/display"})
+    public String DisplayHello(@PathVariable(required = false) String name) {
+        if (name == null) {
             name = "Tikesh";
         }
-        return "Hello "+name+ " from BridgeLabz!";
+        return "Hello " + name + " from BridgeLabz!";
     }
 
+    // Method UC4 using Post method for getting the first and last name
+    // Use http://localhost:8080/getfullname then, in the body {"firstName": "Tikesh","lastName": "Kardate"}
+    @PostMapping("/getfullname")
+    public String sayHello(@RequestBody DtoUser user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+
+    }
 }
